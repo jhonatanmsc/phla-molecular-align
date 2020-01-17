7f5f7ddfa73d2cf3ddf532a9dd13dd2b48fc6a1c
jhonatanmsc <jhonmscosta@gmail.com>
2020-01-17 19:13:08 -0300

    ADDED: load molecules function body

--- app.py
+++ app.py
@@ -6,23 +6,45 @@
     Github: https://github.com/jhonatanmsc/phla-molecular-align
 '''
 
-# load molecules from fasta files
-# returns dict with molecules
-def load_molecules(filename):
+
+def load_molecules(filename: str):
+    '''load molecules from fasta files
+    :param filename: Your file name
+    :returns: dict with molecules
+    :raises: None
+    '''
+    molecules: list
+    with open(filename) as file:
+        lines = file.read()
+        molecules_str = lines.slipt('>')
+        for molecule in molecules_str:
+            molecule = molecule.split('\n', 1)
+            molecules.append({molecule[0]: molecule[1]})
+
+    return molecules
+
+
+def align_molecules(mol1: str, mol2: str):
+    '''Do alignment of two molecules
+    :params mol1, mol2: A name of your molecule
+    :returns: dict with alignment result and indentity
+    :raises: None
+    '''
+    
     return {}
 
-# Do alignment of two molecules
-# returns dict with alignment result and identity
-def align_molecules(mol1, mol2):
-    return {}
 
-# save result of alignment in file.txt
-def save_in_file(filename, alignment):
-    file = open(filename, 'w')
-    for molname in alignment:
-        file.write(alignment[molname]['result'])
-    file.close()
-    return file
+def save_in_file(filename: str, alignment: dict):
+    '''save result of alignment in file.txt
+    :param filename: Your file name
+    :param alignment: A dict with result of alignments of molecules
+    :returns: None
+    :raises: None
+    '''
+
+    with open(filename, 'w') as file:
+        for molname in alignment:
+            file.write(alignment[molname]['result'])
 
 def main():
     molecules = {
@@ -40,10 +62,10 @@ def main():
         alignment = align_molecules(mol1, mol2)
 
         if alignment['identity'] < 1:
-            aligned_err[molname] = alignment  
+            aligned_err[molname] = alignment
         else:
             aligned = alignment
-    
+
     save_in_file('alignment.txt', aligned)
     save_in_file('alignment_err.txt', aligned_err)
 
