c1bcd40970cab813790ca1d90bbad96eefd00949
jhonatanmsc <jhonmscosta@gmail.com>
2020-01-21 16:57:38 -0300

    REMOVED:  Resi class

--- models.py
+++ models.py
@@ -1,24 +1,5 @@
 # import pdb
 
-
-class Resi():
-    '''### Resi Object
-        #### attributes:
-        - pos: aminoacid position
-        - name: aminoacid name abreviated
-    '''
-
-    def __init__(self, pos: int, name: str):
-        self.pos = pos
-        self.name = name
-
-    def __str__(self):
-        return f'<Resi {self.pos} {self.name}'
-
-    def __eq__(self, other):
-        return self.pos == other.pos and self.name == other.name
-
-
 class Molecule():
     '''### Molecule Object
         #### attributes:
@@ -27,24 +8,10 @@ class Molecule():
         - seq: sequence of aminoacids
     '''
 
-    def __init__(self, dbname: str, name: str, seq: list):
+    def __init__(self, dbname: str, name: str, seq: str):
         self.dbname = dbname
         self.name = name
         self.seq = seq
 
     def __str__(self):
         return f'<Molecule {self.dbname} {self.name}>'
-
-    def resi_name(self, pos: int):
-        "#### receive position and returns molecule"
-        for resi in self.seq:
-            if resi.pos == pos:
-                return resi
-        return None
-
-    def all_pos(self):
-        "#### returns all molecule positions"
-        list_pos = []
-        for mol in self.seq:
-            list_pos.append(mol.pos)
-        return list_pos
